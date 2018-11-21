36f7826e26b8911df18d6485785dfe00524cfe4f
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2018-11-21 13:41:30 -0500

    able to click thru different 'pages' added photo box to projects

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
+const app = angular.module('Portfolio', []);
 
+app.controller('MainController', ['$http', function($http){
+
+  this.showSkills = false;
+  this.showProjects = false;
+  this.showPicture = true;
+
+  this.toggleShowSkills = function(){
+    this.showSkills = true;
+    this.showPicture = false;
+    this.showProjects = false;
+  }
+
+  this.toggleShowProjects = function(){
+    this.showProjects = true;
+    this.showPicture = false;
+    this.showSkills = false;
+  }
+
+  this.toggleShowPicture = function(){
+    this.showPicture = true;
+    this.showProjects =false;
+    this.showSkills = false;
+  }
+
+
+}])
